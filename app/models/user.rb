316566926325dae5931e738a9c4eefabee743b06
thibaut gendebien <thibautgendebien@gmail.com>
2017-06-05 19:34:37 +0200

    login with devise

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, :omniauthable, :omniauth_providers => [:facebook]
 
   # has_many :client_orders, :class_name => 'Order', :foreign_key => 'client_id'
   # has_many :waiter_orders, :class_name => 'Order', :foreign_key => 'waiter_id'
