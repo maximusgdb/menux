c7d81801191434b9b81506511423ef040686f743
thibaut gendebien <thibautgendebien@gmail.com>
2017-06-05 22:45:34 +0200

    facebook done

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,16 @@ class User < ApplicationRecord
   # has_many :client_orders, :class_name => 'Order', :foreign_key => 'client_id'
   # has_many :waiter_orders, :class_name => 'Order', :foreign_key => 'waiter_id'
   has_many :orders
+
+  def self.from_omniauth(auth)
+  where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    # user.name = auth.info.name   # assuming the user model has a name
+    # user.image = auth.info.image # assuming the user model has an image
+    # If you are using confirmable and the provider(s) you use validate emails,
+    # uncomment the line below to skip the confirmation emails.
+    # user.skip_confirmation!
+  end
+end
 end
