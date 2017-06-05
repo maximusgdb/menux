992291ac5742a841329f9f884e8f1e9c32db9d0f
Matthieu d'Oultremont <matthieu.doultremont@gmail.com>
2017-06-05 14:56:28 +0200

    removing the validating for description & picture

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -2,8 +2,6 @@ class Bar < ApplicationRecord
   has_many :drinks
   validates :name, presence: true
   validates :address, presence: true
-  validates :description, presence: true
-  validates :picture, presence: true
   validates :opening_hours, presence: true
 
 end
