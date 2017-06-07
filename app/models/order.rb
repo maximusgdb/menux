80b225fe1d2761772021e0c04c04cfec8feb26f3
thibaut gendebien <thibautgendebien@gmail.com>
2017-06-07 15:20:42 +0200

    _footer_order

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,18 @@
 class Order < ApplicationRecord
   belongs_to :client, :class_name => 'User', :foreign_key  => "client_id"
-  belongs_to :waiter, :class_name => 'User', :foreign_key  => "waiter_id"
-  belongs_to :table
++ belongs_to :waiter, :class_name => 'User', :foreign_key  => "waiter_id", optional: true
   has_many :order_lines, dependent: :destroy
+  has_many :drinks, through: :order_lines
+
+  def total_order
+    order_lines.inject(0)  do |total, line|
+      total + (line.quantity * line.drink.price)
+    end
+  end
+
+  def number_of_items_per_order
+    order_lines.inject(0) do |total, line|
+      total + line.quantity
+    end
+  end
 end
