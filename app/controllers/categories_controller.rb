f86f6e429243c31950e11b08600e22cca7119bdb
Stijn Smekens <stijnsmekens@gmail.com>
2017-06-07 14:48:16 +0200

    added controller, no routes yet

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,54 @@
+class CategoriesController < ApplicationController
+  before_action :find_category, only: [:show, :edit, :update, :destroy]
+
+  def index
+    @categories = Category.all
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    #find
+  end
+
+  def edit
+    #find
+  end
+
+  def update
+    #find
+    if @category.update(category_params)
+      flash[:notice] = "Updated"
+      redirect_to categories_path
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    #find
+    @category.destroy
+    redirect_to categories_path
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
+end
