4a2fcf13116a155a33a21078daa37b79ed6a6c33
matthieudou <matthieu.doultremont@gmail.com>
2017-06-08 15:49:09 +0200

    changing the footer and implementing the cart

--- app/controllers/order_lines_controller.rb
+++ app/controllers/order_lines_controller.rb
@@ -3,11 +3,30 @@ class OrderLinesController < ApplicationController
   before_action :find_bar, :find_order
 
   def create
-
+    drink = Drink.find(params[:drink_id])
+    existing_line = @order.order_lines.find_by(drink: drink)
+    if existing_line
+      if params[:remove]
+        if existing_line.quantity - 1 == 0
+          existing_line.destroy
+        else
+          existing_line.quantity -= 1
+          existing_line.save
+        end
+      else
+        existing_line.quantity += 1
+        existing_line.save
+      end
+    else
+      @order.order_lines.create(drink: drink)
+    end
+    if params[:order]
+      redirect_to bar_order_path(@bar, @order)
+    else
+      redirect_to bar_drinks_path(@bar)
+    end
   end
 
-  def update
-  end
 
   private
 
