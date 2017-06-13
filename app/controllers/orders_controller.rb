d1741f75609b6d7c3b84a1b070d9cedf41db652a
thibaut gendebien <thibautgendebien@gmail.com>
2017-06-13 19:10:23 +0200

    problem with bar-id

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -2,6 +2,10 @@ class OrdersController < ApplicationController
 
    before_action :find_bar
 
+  def index
+    @orders = policy_scope(Order)
+  end
+
   def show
     @order = Order.find(params[:id])
     authorize @order
